bc6e476fb75e34ace8446f76b0adaa26517f96b3
Trayambak Rai <59499552+xTrayambak@users.noreply.github.com>
2021-08-15 22:29:55 +0530

    Add color depending on position

--- main.lua
+++ main.lua
@@ -15,18 +15,50 @@ local function startGrid()
 	end
 end
 
+local function initColor(part, X, Z)
+	if X == 1 or Z == 1 then
+		local clone = part:Clone()
+		clone.Position = Vector3.new(clone.Position.X, clone.Position.Y, clone.Position.Z-4)
+		clone.Transparency = .9
+		clone.Name = "FOAM"
+		clone.Parent = workspace.WaterGrid
+		clone.Size = Vector3.new(1, 4, 1)
+	elseif X == GRID_SIZE_X or Z == GRID_SIZE_Z then
+		local clone = part:Clone()
+		clone.Position = Vector3.new(clone.Position.X-4, clone.Position.Y, clone.Position.Z)
+		clone.Transparency = .9
+		clone.Name = "FOAM"
+		clone.Size = Vector3.new(1, 4, 1)
+	end
+end
+
 local function generateGrid()
 	for X = 1, GRID_SIZE_X do
 		for Z = 1, GRID_SIZE_Z do
 			local part = Instance.new("Part", workspace.WaterGrid)
 			part.Anchored = true
 			part.Size = Vector3.new(4, 4, 4)
-			part.Color = Color3.fromRGB(56, 252, 255)
+			part.Color = Color3.fromRGB(56+math.sin(tick()), 252+math.sin(tick()), 255+math.sin(tick()))
 			part.Material = Enum.Material.SmoothPlastic
 			part.Position = Vector3.new(X*4, 0, Z*4)
 			part.Transparency = .6
+			part.Name = "("..X..", "..Z..")"
+			initColor(part, X, Z)
 			grid[X][Z] = part
 		end
+	end
+end
+
+local function updateColors(X, Z, part)
+	if X == 1 and Z == 1 or X == GRID_SIZE_X and Z == GRID_SIZE_Z then
+		part.Transparency = .7
+		part.Color = Color3.fromRGB(121, 255, 251)
+	elseif X > 1 and X < GRID_SIZE_X/2 or Z > 1 and Z < GRID_SIZE_Z/2 then
+		part.Transparency = .6
+		part.Color = Color3.fromRGB(82, 255, 244)
+	else
+		part.Transparency = .6
+		part.Color = Color3.fromRGB(52, 255, 249)
 	end
 end
 
@@ -41,6 +73,8 @@ local function moveGrid()
 				math.sin(tick()+wave_speed),
 				part.Position.Z
 			)
+			
+			updateColors(X, Z, part)
 		end
 	end
 end
